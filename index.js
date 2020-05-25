7a43137fcb86818e4573d1634d64ddf2a61e4edf
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-25 12:13:00 -0700

    add express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi there!')
+const express = require('express');
+
+const app = express(); // An object that describes all the different things that a web server can do
+app.get('/', (req, res) => { // This is a "route handler"
+ res.send('hi there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
