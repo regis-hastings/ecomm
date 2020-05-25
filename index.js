d0d848a89d2d513012707adbc90935754458a2d1
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-25 12:53:01 -0700

    add post route handler

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require('express');
 
 const app = express(); // An object that describes all the different things that a web server can do
 app.get('/', (req, res) => { // This is a "route handler"
- res.send('hi there!');
+ res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <input name="passwordConfirmation" placeholder="password confirmation" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+ `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
