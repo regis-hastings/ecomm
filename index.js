85d0840e1219ab0a7c340bd788400940e1bb69f0
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-25 13:52:19 -0700

    add body-parser library

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); // An object that describes all the different things that a web server can do
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => { // This is a "route handler"
  res.send(`
   <div>
@@ -15,15 +18,7 @@ app.get('/', (req, res) => { // This is a "route handler"
 });
 
 app.post('/', (req, res) => {
-  req.on('data', data => {
-    const parsed = data.toString('utf8').split('&');
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split('=');
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+  console.log(req.body);
   res.send('Account created!!!');
 });
 
