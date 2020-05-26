9ed24a7d265c691f06b1e2876bfb931d72f2338b
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-26 10:43:20 -0700

    add delete function

--- repositories/users.js
+++ repositories/users.js
@@ -43,15 +43,19 @@ class UsersRepository {
     const records = await this.getAll();
     return records.find(record => record.id === id);
   }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
 }
 
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  const user = await repo.getOne('f7');
-
-  console.log(user);
+  await repo.delete('32831175');
 }
 
 test();
