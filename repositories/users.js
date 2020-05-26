8e3ea089083472c32a20dd9335f780e3ba20d4af
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-26 10:01:27 -0700

    add writeAll function

--- repositories/users.js
+++ repositories/users.js
@@ -22,12 +22,17 @@ class UsersRepository {
   }
 
   async create(attrs) {
-    const records = await this.getAll(); // Gets big existing list of users
+    const records = await this.getAll(); 
     records.push(attrs);
-    // write the updated 'records' array back to this.filename
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+
+    await this.writeAll(records);
+  }
+  
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
   }
 }
+
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
