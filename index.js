5b1412ccd5841e2f8e8aec24a31f19bea437ef22
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-27 10:59:00 -0700

    add signin routes

--- index.js
+++ index.js
@@ -9,22 +9,21 @@ app.use(cookieSession({
  keys: ['1zMsv%l2#6VA'] // encryption key
 }));
 
-app.get('/', (req, res) => {
-  // This is a "route handler"
+app.get('/signup', (req, res) => { // This is a "route handler"
   res.send(`
-  <div>
-    Your id is: ${req.session.userId}
-    <form method="POST">
-      <input name="email" placeholder="email" />
-      <input name="password" placeholder="password" />
-      <input name="passwordConfirmation" placeholder="password confirmation" />
-      <button>Sign Up</button>
-    </form>
-  </div>
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
  `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -43,6 +42,41 @@ app.post('/', async (req, res) => {
   req.session.userId = user.id; // session object is added to req by cookieSession
 
   res.send('Account created!!!');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found');
+  }
+
+  if (user.password !== password) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in!');
 });
 
 app.listen(3000, () => {
