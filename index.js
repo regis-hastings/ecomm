a624b52cc6b549a6282224f016e85d97a0ff6bd7
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-27 10:09:02 -0700

    add cookie-session

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
-const app = express(); // An object that describes all the different things that a web server can do
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express(); // An object web server object
+app.use(bodyParser.urlencoded({ extended: true })); // middleware
+app.use(cookieSession({
+ keys: ['1zMsv%l2#6VA'] // encryption key
+}));
 
 app.get('/', (req, res) => {
   // This is a "route handler"
   res.send(`
   <div>
+    Your id is: ${req.session.userId}
     <form method="POST">
       <input name="email" placeholder="email" />
       <input name="password" placeholder="password" />
@@ -30,6 +35,12 @@ app.post('/', async (req, res) => {
   if (password !== passwordConfirmation) {
     return res.send('Passwords must match');
   }
+
+  // Create a user in our user repo to represent this person
+  const user = await usersRepo.create( { email, password });
+
+  // Store the id of that user inside the users cookie
+  req.session.userId = user.id; // session object is added to req by cookieSession
 
   res.send('Account created!!!');
 });
