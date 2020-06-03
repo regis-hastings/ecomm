9aaf3bc1988c18c4ef8413aa9a5a589446985f97
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-06-03 12:21:30 -0700

    add products route handlers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express(); // A web server object
 
@@ -16,6 +17,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
