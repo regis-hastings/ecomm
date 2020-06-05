6f852fdbf968c310c6820d37a56c7c17d6f4c95e
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-06-05 07:16:20 -0700

    add multer for image upload

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,16 @@
+// Require statements getting something from an external library
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
+
+// Require statements getting access to files I authored
 const productsRepo = require('../../repositories/products');
 const newProductTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
+// Declare variables
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -12,11 +18,17 @@ router.get('/admin/products/new', (req, res) => {
   res.send(newProductTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
+router.post(
+  '/admin/products/new',
+  [requireTitle, requirePrice],
+  upload.single('image'),
+  (req, res) => {
+    const errors = validationResult(req);
 
-  res.send('submitted');
-});
+    console.log(req.file);
+
+    res.send('submitted');
+  }
+);
 
 module.exports = router;
