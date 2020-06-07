70e8046251ff8590f0915efec74dbde4e1080830
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-06-07 11:31:00 -0700

    require authentication to access admin panel

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,7 +3,7 @@ const express = require('express');
 const multer = require('multer');
 
 // Require statements getting access to files I authored
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const newProductTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
@@ -13,17 +13,18 @@ const { requireTitle, requirePrice } = require('./validators');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(newProductTemplate({}));
 });
 
 router.post(
   '/admin/products/new',
+  requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(newProductTemplate),
