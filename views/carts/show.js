b519033d07d05658c6254a9c9d4e5807d56ee9d6
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-06-10 11:15:06 -0700

    add logic to calculate total cost

--- views/carts/show.js
+++ views/carts/show.js
@@ -1,6 +1,15 @@
 const layout = require('../layout');
 
 module.exports = ({ items }) => {
+  // let totalPrice = 0;
+  // for (let item of items) {
+  //   totalPrice += item.quantity * item.product.price;
+  // }
+
+  const totalPrice = items.reduce((prev, item) => {
+    return prev + item.quantity * item.product.price;
+  }, 0);
+
   const renderedItems = items
     .map(item => {
       return `
@@ -8,7 +17,7 @@ module.exports = ({ items }) => {
           <h3 class="subtitle">${item.product.title}</h3>
           <div class="cart-right">
             <div>
-              $${item.product.price}  X  ${item.quantity} = 
+              $${item.product.price}  x  ${item.quantity} = 
             </div>
             <div class="price is-size-4">
               $${item.product.price * item.quantity}
@@ -42,7 +51,7 @@ module.exports = ({ items }) => {
               <div class="message-header">
                 Total
               </div>
-              <h1 class="title">$</h1>
+              <h1 class="title">$${totalPrice}</h1>
               <button class="button is-primary">Buy</button>
             </div>
           </div>
